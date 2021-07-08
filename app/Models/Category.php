c060c725cf39295ea5534e0c6d70584b856396ef
Adam Abdurrahman <Adam2802002@gmail.com>
2021-07-08 22:26:53 +0700

    finish table category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'picture'
+    ];
 }
