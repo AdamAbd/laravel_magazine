9ea1465199e19f34ae6f4e8e3e475714dc6304b9
Adam Abdurrahman <Adam2802002@gmail.com>
2021-07-09 18:42:32 +0700

    finish table price

---
+++ app/Http/Controllers/PriceController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Price;
+use Illuminate\Http\Request;
+
+class PriceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Price  $price
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Price $price)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Price  $price
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Price $price)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Price  $price
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Price $price)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_07_09_054232_create_prices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('access');
+            $table->string('time');
+            $table->integer('price');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
