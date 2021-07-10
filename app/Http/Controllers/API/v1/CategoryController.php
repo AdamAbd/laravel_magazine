319a29a7496b6a112852fc6aa3c511d661f3dec8
Adam Abdurrahman <Adam2802002@gmail.com>
2021-07-10 14:52:34 +0700

    Update to use api versioning

---
+++ app/Http/Controllers/API/v1/CategoryController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        return Category::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Category::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+        $category->update($request->all());
+        return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Category::destroy($id);
+        return $product;
+    }
+
+    public function search(Request $request)
+    {
+        $name = $request->input('name');
+
+        $category = Category::query();
+        $category->where('name', 'like', '%' . $name . '%');
+
+        return $category->take(1)->get();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\API\v1\CategoryController;
 use App\Http\Controllers\MagazineController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
